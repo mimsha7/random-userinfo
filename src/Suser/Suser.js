1ad4e9045769957c8891807388b230a21f23d6c7
Md.Mimshadur Rahman <rahman15-6119@diu.edu.bd>
2022-08-17 12:30:32 +0600

    updated

--- src/Suser/Suser.js
+++ src/Suser/Suser.js
@@ -3,12 +3,19 @@ import './Suser.css'
 
 const Suser = (props) => {
     const suser = props.suser;
-    let totalSalary = suser.reduce((total, user) => total+user.salary, 0);
-    // console.log(totalSalary);
+    let totalSalary = suser.reduce((total, user) => total+ Number(user.salary), 0);
     return (
         <div className="total-info">
-            <h2>Total Count: {suser.length}</h2>
+            <h2>Total Added: {suser.length}</h2>
             <h3>Total Salary: ${totalSalary}</h3>
+            {
+                suser.map((user) => {
+                return(
+                       <h3>Name: {user.name} <img style={{alignItem: 'center'}} src={user.image} alt="" width="10%" /></h3> 
+                    )
+                })
+            }
+            
         </div>
     );
 };
